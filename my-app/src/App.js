2d3e7d3acdffc5c07196649dd254bba62a55e257
Dom <163606370+DomHouricanTraining@users.noreply.github.com>
2024-10-12 17:36:55 +0000

    Chpt1 Done

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,8 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Counter from "./components/Counter";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 
 const App = () => {
-  return <div></div>;
+  return <div>
+      <Counter />
+      <h1>Hello World of React!</h1>
+  </div>;
 };
 
 export default App;
